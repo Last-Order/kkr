e45d930ce639a5ff65413361ed1e511de755e61d
Eridanus Sora <sora@sound.moe>
2020-06-18 20:17:30 +0800

    add response length check

--- src/utils/download_file.ts
+++ src/utils/download_file.ts
@@ -1,6 +1,6 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import axios from 'axios';
+import * as fs from "fs";
+import axios from "axios";
+export class BadResponseError extends Error {}
 const download = async (url, dest, { timeout }): Promise<void> => {
     const CancelToken = axios.CancelToken;
     const source = CancelToken.source();
@@ -9,12 +9,18 @@ const download = async (url, dest, { timeout }): Promise<void> => {
     }, timeout || 60000);
     const response = await axios({
         url,
-        method: 'GET',
-        responseType: 'arraybuffer',
+        method: "GET",
+        responseType: "arraybuffer",
         cancelToken: source.token,
     });
+    if (
+        response.headers["content-length"] &&
+        +response.headers["content-length"] !== response.data.length
+    ) {
+        throw new BadResponseError("下载内容不完整");
+    }
     clearTimeout(timer);
     return fs.writeFileSync(dest, response.data);
-}
+};
 
 export default download;
