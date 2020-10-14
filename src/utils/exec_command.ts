42324167d8c27f1b53f2284df63502134db9deb5
fireattack <human.peng@gmail.com>
2020-10-13 20:56:31 -0500

    Fix typo

--- src/utils/exec_command.ts
+++ src/utils/exec_command.ts
@@ -1,18 +1,18 @@
 const exec = require('child_process').exec;
-const execCommand = (command: string, slient: boolean = false) => {
+const execCommand = (command: string, silent: boolean = false) => {
     return new Promise((resolve, reject) => {
         let child = exec(command);
         child.stdout.on('data', (data) => {
-            !slient && console.log(data);
+            !silent && console.log(data);
         });
         child.stderr.on('data', (data) => {
-            !slient && console.log(data);
+            !silent && console.log(data);
         });
         child.on('close', (code, signal) => {
             if (code === 0) {
                 resolve();
             } else {
-                !slient && console.error(code, signal);
+                !silent && console.error(code, signal);
                 reject();
             }
         }); 
